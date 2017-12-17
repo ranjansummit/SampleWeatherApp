8c0940e669195a1dc9da1ffcbd7a0024dcf5a0df
ranjansummit <ranjan.summit@gmail.com>
2017-12-17 09:58:56 +0545

    l

---
+++ source/repos/WeatherForeCasting/WeatherForeCasting/WeatherForeCasting/DataRetrieve.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WeatherForeCasting
+{
+    public class DataRetrieve
+    {
+        public static async Task<Weather> GetWeather(string zipCode)
+        {
+             
+            string key = "1e1a77eb73156f743f2431bb71aed395";
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?zip="
+                + zipCode + "&appid=" + key;
+
+            var results = await DataService.getDataFromService(queryString).ConfigureAwait(false);
+
+            if (results["weather"] != null)
+            {
+                Weather weather = new Weather();
+                weather.Title = (string)results["name"];
+                weather.Temperature = (string)results["main"]["temp"] + " F";
+                weather.Wind = (string)results["wind"]["speed"] + " mph";
+                weather.Humidity = (string)results["main"]["humidity"] + " %";
+                weather.Visibility = (string)results["weather"][0]["main"];
+
+                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
+                weather.Sunrise = sunrise.ToString() + " UTC";
+                weather.Sunset = sunset.ToString() + " UTC";
+                return weather;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ source/repos/WeatherForeCasting/WeatherForeCasting/WeatherForeCasting/WeatherInfo.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WeatherForeCasting
+{
+    public partial class WeatherInfo : ContentPage
+    {
+        public WeatherInfo()
+        {
+            InitializeComponent();
+            this.Title = "Ranjan's Weather App";
+            getWeatherBtn.Clicked += GetWeatherBtn_Clicked;
+
+            //Set the default binding to a default object for now
+            this.BindingContext = new Weather();
+        }
+
+        private async void GetWeatherBtn_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(zipCodeEntry.Text))
+            {
+                Weather weather = await DataRetrieve.GetWeather(zipCodeEntry.Text);
+                if (weather != null)
+                {
+                    this.BindingContext = weather;
+                    getWeatherBtn.Text = "Search Again";
+                }
+            }
+        }
+    }
+}
